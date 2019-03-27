4ab9c143210a1e7aa4a01c772371f892f8df3f0c
Maxim Kucherov <maxima-net@bk.ru>
2019-03-27 21:46:37 +0300

    Use ts for electron start scripts

---
+++ src/electron-wait-react.ts
@@ -0,0 +1,26 @@
+import net from 'net';
+import {exec} from 'child_process';
+
+const port = process.env.PORT ? (+process.env.PORT - 100) : 3000;
+
+process.env.ELECTRON_START_URL = `http://localhost:${port}`;
+
+const client = new net.Socket();
+
+let startedElectron = false;
+const tryConnection = () => client.connect({port: port}, () => {
+        client.end();
+        if(!startedElectron) {
+            console.log('starting electron');
+            startedElectron = true;
+            exec('npm run electron');
+        }
+    }
+);
+
+tryConnection();
+
+client.on('error', (error) => {
+    setTimeout(tryConnection, 1000);
+});
+    
