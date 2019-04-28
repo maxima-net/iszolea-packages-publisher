a8365cc71063f01e3f8869cedc8ba510b0d3a00d
Maxim Kucherov <maxima-net@bk.ru>
2019-04-28 13:12:43 +0300

    Install react and redux extensions inside the electron app

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers/index';
 import thunk from 'redux-thunk';
 
-const store = createStore(
-  rootReducer,
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(
   applyMiddleware(thunk)
-);
+));
 
 export default store;
