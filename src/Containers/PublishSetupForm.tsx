1d375150b65109d028519fed744b16b79e02b6e9
Maxim Kucherov <maxima-net@bk.ru>
2019-11-05 01:34:11 +0300

    Improve current branch displaying

--- src/Containers/PublishSetupForm.tsx
+++ src/Containers/PublishSetupForm.tsx
@@ -143,7 +143,7 @@ class PublishSetupForm extends PureComponent<PublishSetupFormProps> {
               />
               <span className="status-container__status">{isEverythingCommittedInputText}</span>
             </label>
-            <label className="branch-container" title="Current branch name">
+            <label className="branch-container" style={{ visibility: this.props.branchName ? undefined : 'hidden' }} title="Current branch name">
               <i className="material-icons git-info__icon">call_split</i>
               <span className="branch-container__branch">{this.props.branchName}</span>
             </label>
