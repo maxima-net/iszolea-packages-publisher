df77d1882ec2bc1da68163622015ae7077f5e3a5
Maxim Kucherov <maxima-net@bk.ru>
2019-11-16 22:47:20 +0300

    Refactor Config

--- src/store/settings/actions.ts
+++ src/store/settings/actions.ts
@@ -1,49 +1,53 @@
-import * as Config from '../../utils/config';
 import { SettingsKeys, SettingsFields, Settings, AppState } from '../types';
 import { ThunkAction } from 'redux-thunk';
 import { decrypt, encrypt } from '../../utils/encryption';
 import { validateSettings } from '../../utils/settings';
 import { SettingsAction } from './types';
 import { LayoutAction } from '../layout/types';
+import Config from '../../utils/config';
 
 export const loadSettings = () => {
+  const config = new Config();
+  
   const settingsFields: SettingsFields = {
-    nuGetApiKey: decrypt(Config.Get<string>(SettingsKeys.NuGetApiKey)),
+    nuGetApiKey: decrypt(config.Get<string>(SettingsKeys.NuGetApiKey)),
 
-    isIszoleaPackagesIncluded: Config.Get<boolean>(SettingsKeys.IsIszoleaPackagesIncluded, false),
-    baseSlnPath: Config.Get<string>(SettingsKeys.BaseSlnPath),
+    isIszoleaPackagesIncluded: config.Get<boolean>(SettingsKeys.IsIszoleaPackagesIncluded, false),
+    baseSlnPath: config.Get<string>(SettingsKeys.BaseSlnPath),
 
-    isBomCommonPackageIncluded: Config.Get<boolean>(SettingsKeys.IsBomCommonPackageIncluded),
-    bomCommonPackageSlnPath: Config.Get<string>(SettingsKeys.BomCommonPackageSlnPath),
+    isBomCommonPackageIncluded: config.Get<boolean>(SettingsKeys.IsBomCommonPackageIncluded),
+    bomCommonPackageSlnPath: config.Get<string>(SettingsKeys.BomCommonPackageSlnPath),
 
-    isIszoleaUiPackageIncluded: Config.Get<boolean>(SettingsKeys.IsIszoleaUiPackageIncluded, false),
-    uiPackageJsonPath: Config.Get<string>(SettingsKeys.UiPackageJsonPath),
+    isIszoleaUiPackageIncluded: config.Get<boolean>(SettingsKeys.IsIszoleaUiPackageIncluded, false),
+    uiPackageJsonPath: config.Get<string>(SettingsKeys.UiPackageJsonPath),
 
-    npmAutoLogin: Config.Get<boolean>(SettingsKeys.NpmAutoLogin, false),
-    npmLogin: Config.Get<string>(SettingsKeys.NpmLogin),
-    npmPassword: decrypt(Config.Get<string>(SettingsKeys.NpmPassword)),
-    npmEmail: Config.Get<string>(SettingsKeys.NpmEmail)
+    npmAutoLogin: config.Get<boolean>(SettingsKeys.NpmAutoLogin, false),
+    npmLogin: config.Get<string>(SettingsKeys.NpmLogin),
+    npmPassword: decrypt(config.Get<string>(SettingsKeys.NpmPassword)),
+    npmEmail: config.Get<string>(SettingsKeys.NpmEmail)
   };
 
   return applySettingsCore(settingsFields);
 };
 
 export const applySettings = (settingsFields: SettingsFields) => {
-  Config.Set(SettingsKeys.IsIszoleaPackagesIncluded, !!settingsFields.isIszoleaPackagesIncluded);
-  Config.Set(SettingsKeys.BaseSlnPath, settingsFields.baseSlnPath || '');
-  
-  Config.Set(SettingsKeys.IsBomCommonPackageIncluded, !!settingsFields.isBomCommonPackageIncluded);
-  Config.Set(SettingsKeys.BomCommonPackageSlnPath, settingsFields.bomCommonPackageSlnPath || '');
-  
-  Config.Set(SettingsKeys.NuGetApiKey, encrypt(settingsFields.nuGetApiKey || ''));
+  const config = new Config();
 
-  Config.Set(SettingsKeys.IsIszoleaUiPackageIncluded, !!settingsFields.isIszoleaUiPackageIncluded);
-  Config.Set(SettingsKeys.UiPackageJsonPath, settingsFields.uiPackageJsonPath || '');
+  config.Set(SettingsKeys.IsIszoleaPackagesIncluded, !!settingsFields.isIszoleaPackagesIncluded);
+  config.Set(SettingsKeys.BaseSlnPath, settingsFields.baseSlnPath || '');
   
-  Config.Set(SettingsKeys.NpmAutoLogin, !!settingsFields.npmAutoLogin);
-  Config.Set(SettingsKeys.NpmLogin, settingsFields.npmLogin || '');
-  Config.Set(SettingsKeys.NpmPassword, encrypt(settingsFields.npmPassword || ''));
-  Config.Set(SettingsKeys.NpmEmail, settingsFields.npmEmail || '');
+  config.Set(SettingsKeys.IsBomCommonPackageIncluded, !!settingsFields.isBomCommonPackageIncluded);
+  config.Set(SettingsKeys.BomCommonPackageSlnPath, settingsFields.bomCommonPackageSlnPath || '');
+  
+  config.Set(SettingsKeys.NuGetApiKey, encrypt(settingsFields.nuGetApiKey || ''));
+
+  config.Set(SettingsKeys.IsIszoleaUiPackageIncluded, !!settingsFields.isIszoleaUiPackageIncluded);
+  config.Set(SettingsKeys.UiPackageJsonPath, settingsFields.uiPackageJsonPath || '');
+  
+  config.Set(SettingsKeys.NpmAutoLogin, !!settingsFields.npmAutoLogin);
+  config.Set(SettingsKeys.NpmLogin, settingsFields.npmLogin || '');
+  config.Set(SettingsKeys.NpmPassword, encrypt(settingsFields.npmPassword || ''));
+  config.Set(SettingsKeys.NpmEmail, settingsFields.npmEmail || '');
 
   return applySettingsCore(settingsFields);
 };
