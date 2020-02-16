516196f26f5d8e087c64f883e8b0194bfa38420f
Maxim Kucherov <maxima-net@bk.ru>
2020-02-17 01:21:09 +0300

    Refactor PublishExecutingView

--- src/Containers/PublishExecutingView.tsx
+++ src/Containers/PublishExecutingView.tsx
@@ -1,126 +1,85 @@
-import React, { PureComponent } from 'react';
-import { MapStateToPropsParam, connect } from 'react-redux';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { updatePublishingInfo, rejectPublishing, publishPackage, pushWithTags } from '../store/publishing/actions';
-import { PublishingInfo, AppState } from '../store/types';
+import { AppState, Publishing } from '../store/types';
 import { PublishingGlobalStage, PublishingStageStatus, PublishingStage } from '../store/publishing/types';
 import CheckRow from '../Components/CheckRow';
 import ProgressBar from '../Components/ProgressBar';
 import ErrorRow from '../Components/ErrorRow';
 import ViewContainer from '../Components/ViewContainer';
-import ProjectInfo from '../packages/project-info';
 import './PublishExecutingView.scss';
 import Button from '../Components/Button';
 import ConfirmDialog from '../Components/ConfirmDialog';
 import config from '../config.json';
 
-interface MappedProps {
-  projectInfo: ProjectInfo[] | undefined;
-  packageVersion: string;
-  publishingInfo: PublishingInfo | undefined;
-}
+const PublishExecutingView: React.FC = () => {
+  const confirmRejectDialog = React.createRef<ConfirmDialog>();
+  const dispatch = useDispatch();
 
-const mapStateToProps: MapStateToPropsParam<MappedProps, any, AppState> = (state) => {
-  return {
-    projectInfo: state.publishing.selectedPackageSet && state.publishing.selectedPackageSet.projectsInfo,
-    packageVersion: state.publishing.newVersion,
-    publishingInfo: state.publishing.publishingInfo
-  };
-};
+  const publishing = useSelector<AppState, Publishing>((state) => state.publishing);
 
-interface Dispatchers {
-  updatePublishingInfo: (publishingInfo: PublishingInfo | undefined) => void;
-  rejectPublishing: () => void;
-  publishPackage: () => void;
-  pushWithTags: () => void;
-}
+  const { newVersion, publishingInfo, selectedPackageSet } = publishing;
+  const projectInfo = selectedPackageSet && selectedPackageSet.projectsInfo;
 
-const dispatchers: Dispatchers = {
-  updatePublishingInfo,
-  rejectPublishing,
-  publishPackage,
-  pushWithTags
-};
-
-type PublishExecutingViewProps = MappedProps & Dispatchers;
-
-class PublishExecutingView extends PureComponent<PublishExecutingViewProps> {
-  confirmRejectDialog = React.createRef<ConfirmDialog>();
-
-  render() {
-    if (!this.props.projectInfo || !this.props.publishingInfo) {
-      return <h2>Publishing info is not available</h2>;
-    }
-
-    const packages = this.props.projectInfo.map((i) => i.name);
-    const packagesList = packages.map((p) => {
-      return `${p}.${this.props.packageVersion}`;
-    }).join(', ');
-
-    const { error, globalStage, stages } = this.props.publishingInfo;
-    const isExecuting = globalStage === PublishingGlobalStage.Publishing
-      || globalStage === PublishingGlobalStage.Rejecting
-      || globalStage === PublishingGlobalStage.Pushing;
-
-    const stagesItems = Array.from(this.props.publishingInfo.stages)
-      .map(([k, v]) => (
-        <CheckRow
-          key={k}
-          isChecked={v.status === PublishingStageStatus.Finished}
-          isBlinking={v.status === PublishingStageStatus.Executing}
-          isInvalid={v.status === PublishingStageStatus.Failed}
-          text={`${v.text}${v.status === PublishingStageStatus.Executing ? '...' : ''}`}
-        />)
-      );
-
-    const isPublished = globalStage === PublishingGlobalStage.Published;
-    const isPublishedButNotPushed = globalStage === PublishingGlobalStage.Published && !stages.has(PublishingStage.GitPush);
-    const isFailed = !!error;
-
-    return (
-      <>
-        <ViewContainer>
-          <h5>{packagesList}</h5>
-          <ErrorRow text={error} isVisible={isFailed} />
-          <ProgressBar isVisible={isExecuting} />
-          {stagesItems}
-          <div className="row row-publishing-buttons" style={{ display: isExecuting ? 'none' : undefined }}>
-            <Button text="Ok, thanks" onClick={this.handleCloseClick} icon="done" color="blue" />
-            <Button text="Retry" onClick={this.handleRetryClick} icon="replay" color="blue" isHidden={!isFailed} />
-            <Button text="Git: Push with tags" onClick={this.pushWithTags} icon="publish" color="blue" isHidden={!isPublishedButNotPushed} />
-            <Button text="UnPublish" onClick={this.showConfirmRejectDialog} icon="clear" color="red" isHidden={!isPublished} />
-          </div>
-        </ViewContainer>
-        <ConfirmDialog
-          ref={this.confirmRejectDialog}
-          title="Confirm rejection" 
-          text={config.texts.confirmRejectionText}
-          confirmButtonText="Reject" 
-          onConfirm={this.handleRejectClick}
-          isModal={true}
-        />
-      </>
-    );
+  if (!projectInfo || !publishingInfo) {
+    return <h2>Publishing info is not available</h2>;
   }
 
-  private showConfirmRejectDialog = () => {
-    this.confirmRejectDialog.current && this.confirmRejectDialog.current.show();
-  };
+  const packages = projectInfo.map((i) => i.name);
+  const packagesList = packages.map((p) => {
+    return `${p}.${newVersion}`;
+  }).join(', ');
 
-  private handleCloseClick = () => {
-    this.props.updatePublishingInfo(undefined);
-  };
+  const { error, globalStage, stages } = publishingInfo;
+  const isExecuting = globalStage === PublishingGlobalStage.Publishing
+    || globalStage === PublishingGlobalStage.Rejecting
+    || globalStage === PublishingGlobalStage.Pushing;
 
-  private handleRetryClick = () => {
-    this.props.publishPackage();
-  };
+  const stagesItems = Array.from(publishingInfo.stages)
+    .map(([k, v]) => (
+      <CheckRow
+        key={k}
+        isChecked={v.status === PublishingStageStatus.Finished}
+        isBlinking={v.status === PublishingStageStatus.Executing}
+        isInvalid={v.status === PublishingStageStatus.Failed}
+        text={`${v.text}${v.status === PublishingStageStatus.Executing ? '...' : ''}`}
+      />)
+    );
 
-  private handleRejectClick = () => {
-    this.props.rejectPublishing();
-  };
+  const isPublished = globalStage === PublishingGlobalStage.Published;
+  const isPublishedButNotPushed = globalStage === PublishingGlobalStage.Published && !stages.has(PublishingStage.GitPush);
+  const isFailed = !!error;
 
-  private pushWithTags = () => {
-    this.props.pushWithTags();
-  };
-}
+  const showConfirmRejectDialog = () => confirmRejectDialog.current && confirmRejectDialog.current.show();
+  const handleCloseClick = () => dispatch(updatePublishingInfo(undefined));
+  const handleRetryClick = () => dispatch(publishPackage());
+  const handleRejectClick = () => dispatch(rejectPublishing());
+  const handlePushWithTagsClick = () => dispatch(pushWithTags());
 
-export default connect(mapStateToProps, dispatchers)(PublishExecutingView);
+  return (
+    <>
+      <ViewContainer>
+        <h5>{packagesList}</h5>
+        <ErrorRow text={error} isVisible={isFailed} />
+        <ProgressBar isVisible={isExecuting} />
+        {stagesItems}
+        <div className="row row-publishing-buttons" style={{ display: isExecuting ? 'none' : undefined }}>
+          <Button text="Ok, thanks" onClick={handleCloseClick} icon="done" color="blue" />
+          <Button text="Retry" onClick={handleRetryClick} icon="replay" color="blue" isHidden={!isFailed} />
+          <Button text="Git: Push with tags" onClick={handlePushWithTagsClick} icon="publish" color="blue" isHidden={!isPublishedButNotPushed} />
+          <Button text="UnPublish" onClick={showConfirmRejectDialog} icon="clear" color="red" isHidden={!isPublished} />
+        </div>
+      </ViewContainer>
+      <ConfirmDialog
+        ref={confirmRejectDialog}
+        title="Confirm rejection"
+        text={config.texts.confirmRejectionText}
+        confirmButtonText="Reject"
+        onConfirm={handleRejectClick}
+        isModal={true}
+      />
+    </>
+  );
+};
+
+export default PublishExecutingView;
