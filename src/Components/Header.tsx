121e3770533494269fc902118ed86e59122a350c
Maxim Kucherov <maxima-net@bk.ru>
2020-02-17 00:59:10 +0300

    Refactor Header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,46 +2,39 @@ import React from 'react';
 import './Header.scss';
 import { switchSettingsView } from '../store/layout/actions';
 import Log from 'electron-log';
-import { MapStateToPropsParam, connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { AppState, UpdateStatus } from '../store/types';
 import { shell } from 'electron';
 import config from '../config.json';
 import routes from '../routes';
 
-interface MappedProps {
-  isSettingsActive: boolean;
-  isSettingsSwitchHidden: boolean;
-}
-
-const mapStateToProps: MapStateToPropsParam<MappedProps, any, AppState> = (state) => {
-  const isInitializing = state.initialization.isInitialized !== true;
-  const isUpdating = state.layout.updateStatus !== UpdateStatus.DeclinedByUser
-    && state.layout.updateStatus !== UpdateStatus.UpdateIsNotAvailable;
-  const isPublishing = !!state.publishing.publishingInfo;
-
-  return {
-    isSettingsActive: state.router.location.pathname === routes.settings,
-    isSettingsSwitchHidden: isUpdating || isInitializing || isPublishing
-  };
-};
-
-interface Dispatchers {
-  switchSettingsView(value: boolean): void;
-}
-
-const dispatchers: Dispatchers = {
-  switchSettingsView
-};
-
-interface OwnProps {
+interface HeaderProps {
   title: string;
-  isLogoCentered: boolean;
+  isLogoCentered?: boolean;
 }
-
-type HeaderProps = MappedProps & Dispatchers & OwnProps;
 
 const Header: React.FC<HeaderProps> = (props) => {
-  const settingsLinkClass = props.isSettingsActive ? 'active' : undefined;
+  const dispatch = useDispatch();
+  const onSettingsClick = () => {
+    dispatch(switchSettingsView(!isSettingsActive));
+  };
+
+  const locationPath = useSelector<AppState, string>((state) => state.router.location.pathname);
+  const isSettingsActive = locationPath === routes.settings;
+  const settingsLinkClass = isSettingsActive ? 'active' : undefined;
+
+  const isInitializing = useSelector<AppState, boolean>((state) => state.initialization.isInitialized !== true);
+  const isUpdating = useSelector<AppState, boolean>((state) => {
+    const { updateStatus } = state.layout;
+    return updateStatus !== UpdateStatus.DeclinedByUser && updateStatus !== UpdateStatus.UpdateIsNotAvailable;
+  });
+  const isPublishing = useSelector<AppState, boolean>((state) => !!state.publishing.publishingInfo);
+  const isSettingsSwitchHidden = isUpdating || isInitializing || isPublishing;
+
+  const openLog = () => {
+    const logFilePath = Log.transports.file.findLogPath();
+    shell.openExternal(logFilePath);
+  };
 
   return (
     <nav>
@@ -64,8 +57,8 @@ const Header: React.FC<HeaderProps> = (props) => {
                 href="#"
                 tabIndex={-1}
                 title="Settings"
-                hidden={props.isSettingsSwitchHidden}
-                onClick={() => props.switchSettingsView(!props.isSettingsActive)}>
+                hidden={isSettingsSwitchHidden}
+                onClick={onSettingsClick}>
                 <i className="material-icons">settings</i>
               </a>
             </li>
@@ -73,9 +66,7 @@ const Header: React.FC<HeaderProps> = (props) => {
               <a
                 href="#"
                 tabIndex={-1}
-                title="How to use"
-                hidden={props.isSettingsSwitchHidden}
-                onClick={() => props.switchSettingsView(!props.isSettingsActive)}>
+                title="Theme"
                 <i className="material-icons">brightness_4</i>
               </a>
             </li> */}
@@ -86,7 +77,7 @@ const Header: React.FC<HeaderProps> = (props) => {
                 rel="noopener noreferrer"
                 tabIndex={-1}
                 title="How to use"
-                hidden={props.isSettingsSwitchHidden}>
+                hidden={isSettingsSwitchHidden}>
                 <i className="material-icons">help</i>
               </a>
             </li>
@@ -95,11 +86,6 @@ const Header: React.FC<HeaderProps> = (props) => {
       </div>
     </nav>
   );
-}
+};
 
-function openLog() {
-  const logFilePath = Log.transports.file.findLogPath();
-  shell.openExternal(logFilePath);
-}
-
-export default connect(mapStateToProps, dispatchers)(Header);
+export default Header;
