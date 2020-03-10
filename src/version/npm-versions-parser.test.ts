9860a35c2920c9b349569b12f96841cd31746439
Maxim Kucherov <maxima-net@bk.ru>
2020-03-10 22:05:13 +0300

    Add tests for npm versions parsing

---
+++ src/version/npm-versions-parser.test.ts
@@ -0,0 +1,41 @@
+import { parseVersionsList } from './npm-versions-parser';
+import { PackageVersionInfo } from './nuget-versions-parser';
+
+it('parses npm version list', () => {
+  const rawVersions = `[ '0.0.1',
+  '0.4.0-beta.8',
+  '0.4.0-beta9',
+  '0.4.0.9',
+  '0.4.0' ]`;
+
+  const expectedResult: PackageVersionInfo[] = [
+    { isValid: true, rawVersion: '0.0.1', parsedVersion: { major: 0, minor: 0, patch: 1, betaIndex: undefined } },
+    { isValid: true, rawVersion: '0.4.0-beta.8', parsedVersion: { major: 0, minor: 4, patch: 0, betaIndex: 8 } },
+    { isValid: false, rawVersion: '0.4.0-beta9', parsedVersion: { major: 0, minor: 4, patch: 0, betaIndex: 9 } },
+    { isValid: false, rawVersion: '0.4.0.9', parsedVersion: undefined },
+    { isValid: true, rawVersion: '0.4.0', parsedVersion: { major: 0, minor: 4, patch: 0, betaIndex: undefined } },
+  ];
+
+  const result = parseVersionsList(rawVersions);
+
+  expect(result).toStrictEqual(expectedResult);
+});
+
+it('parses npm version list with multiple versions in a row', () => {
+  const rawVersions = `[
+    '0.0.49',         '0.0.50-beta.0',  '0.0.50-beta1',  '0.0.50',
+    '0.4.0-beta.28'
+  ]`;
+
+  const expectedResult: PackageVersionInfo[] = [
+    { isValid: true, rawVersion: '0.0.49', parsedVersion: { major: 0, minor: 0, patch: 49, betaIndex: undefined } },
+    { isValid: true, rawVersion: '0.0.50-beta.0', parsedVersion: { major: 0, minor: 0, patch: 50, betaIndex: 0 } },
+    { isValid: false, rawVersion: '0.0.50-beta1', parsedVersion: { major: 0, minor: 0, patch: 50, betaIndex: 1 } },
+    { isValid: true, rawVersion: '0.0.50', parsedVersion: { major: 0, minor: 0, patch: 50, betaIndex: undefined } },
+    { isValid: true, rawVersion: '0.4.0-beta.28', parsedVersion: { major: 0, minor: 4, patch: 0, betaIndex: 28 } },
+  ];
+
+  const result = parseVersionsList(rawVersions);
+
+  expect(result).toStrictEqual(expectedResult);
+});
