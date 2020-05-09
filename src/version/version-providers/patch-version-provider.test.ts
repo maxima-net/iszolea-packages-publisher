68c92516e2a3323f4685e24267dca6d99f573740
Maxim Kucherov <maxima-net@bk.ru>
2020-05-09 21:05:26 +0300

    Improve patch version provider

--- src/version/version-providers/patch-version-provider.test.ts
+++ src/version/version-providers/patch-version-provider.test.ts
@@ -1,5 +1,6 @@
 import PatchVersionProvider from './patch-version-provider';
 import { TestCase, TargetVersionDescription } from '.';
+import { PackageVersionInfo } from '../nuget-versions-parser';
 
 it('returns target version and new version', () => {
   const testCases: TestCase[] = [
@@ -17,6 +18,115 @@ it('returns target version and new version', () => {
   
   testCases.forEach((t) => {
     const provider = new PatchVersionProvider(t.current, []);
+    const tvInfo = provider.getTargetVersion();
+    expect(tvInfo).toStrictEqual(t.expectedTarget);
+
+    const newVersion = provider.getNewVersionString();
+    expect(newVersion).toBe(t.expectedNew);
+  });
+});
+
+it('returns target version and new version  with published version info', () => {
+  const publishedVersions: PackageVersionInfo[] = [
+    { parsedVersion: { major: 11, minor: 22, patch: 4, betaIndex: undefined },  rawVersion: '11.22.4',        isValid: true },
+
+    { parsedVersion: { major: 11, minor: 22, patch: 5, betaIndex: 1 },          rawVersion: '11.22.5-beta-1', isValid: true },
+    { parsedVersion: { major: 11, minor: 22, patch: 5, betaIndex: 2 },          rawVersion: '11.22.5-beta-2', isValid: true },
+    { parsedVersion: { major: 11, minor: 22, patch: 5, betaIndex: undefined },  rawVersion: '11.22.5',        isValid: true },
+
+    { parsedVersion: { major: 11, minor: 22, patch: 6, betaIndex: 1 },          rawVersion: '11.22.6-beta-1', isValid: true },
+    { parsedVersion: { major: 11, minor: 22, patch: 6, betaIndex: undefined },  rawVersion: '11.22.6',        isValid: true },
+
+    { parsedVersion: { major: 11, minor: 22, patch: 8, betaIndex: undefined },  rawVersion: '11.22.8',        isValid: true }
+  ];
+
+  const testCases: TestCase[] = [
+    { 
+      current: '11.22.2',
+      expectedTarget: { version: { major: 11, minor: 22, patch: 8, suffix: undefined }, description: TargetVersionDescription.LATEST_PUBLISHED_PATCH_VERSION },
+      expectedNew: '11.22.9' 
+    },
+    { 
+      current: '11.22.3',
+      expectedTarget: { version: { major: 11, minor: 22, patch: 8, suffix: undefined }, description: TargetVersionDescription.LATEST_PUBLISHED_PATCH_VERSION },
+      expectedNew: '11.22.9' 
+    },
+    { 
+      current: '11.22.6',
+      expectedTarget: { version: { major: 11, minor: 22, patch: 8, suffix: undefined }, description: TargetVersionDescription.LATEST_PUBLISHED_PATCH_VERSION },
+      expectedNew: '11.22.9' 
+    },
+    { 
+      current: '11.22.7',
+      expectedTarget: { version: { major: 11, minor: 22, patch: 8, suffix: undefined }, description: TargetVersionDescription.LATEST_PUBLISHED_PATCH_VERSION },
+      expectedNew: '11.22.9' 
+    },
+    { 
+      current: '11.22.8',
+      expectedTarget: { version: { major: 11, minor: 22, patch: 8, suffix: undefined }, description: TargetVersionDescription.LOCAL_VERSION },
+      expectedNew: '11.22.9' 
+    },
+    { 
+      current: '11.22.9',
+      expectedTarget: { version: { major: 11, minor: 22, patch: 9, suffix: undefined }, description: TargetVersionDescription.LOCAL_VERSION },
+      expectedNew: '11.22.10' 
+    },
+  ];
+
+  testCases.forEach((t) => {
+    const provider = new PatchVersionProvider(t.current, publishedVersions);
+    const tvInfo = provider.getTargetVersion();
+    expect(tvInfo).toStrictEqual(t.expectedTarget);
+
+    const newVersion = provider.getNewVersionString();
+    expect(newVersion).toBe(t.expectedNew);
+  });
+});
+
+it('returns target version and new version for current beta with published version info', () => {
+  const publishedVersions: PackageVersionInfo[] = [
+    { parsedVersion: { major: 11, minor: 22, patch: 4, betaIndex: undefined },  rawVersion: '11.22.4',        isValid: true },
+
+    { parsedVersion: { major: 11, minor: 22, patch: 5, betaIndex: 1 },          rawVersion: '11.22.5-beta-1', isValid: true },
+    { parsedVersion: { major: 11, minor: 22, patch: 5, betaIndex: 2 },          rawVersion: '11.22.5-beta-2', isValid: true },
+    { parsedVersion: { major: 11, minor: 22, patch: 5, betaIndex: undefined },  rawVersion: '11.22.5',        isValid: true },
+
+    { parsedVersion: { major: 11, minor: 22, patch: 6, betaIndex: 1 },          rawVersion: '11.22.6-beta-1', isValid: true },
+    { parsedVersion: { major: 11, minor: 22, patch: 6, betaIndex: undefined },  rawVersion: '11.22.6',        isValid: true },
+
+    { parsedVersion: { major: 11, minor: 22, patch: 8, betaIndex: undefined },  rawVersion: '11.22.8',        isValid: true }
+  ];
+
+  const testCases: TestCase[] = [
+    { 
+      current: '11.22.3-beta.4',
+      expectedTarget: { version: { major: 11, minor: 22, patch: 3, suffix: 'beta.4' }, description: TargetVersionDescription.LOCAL_VERSION },
+      expectedNew: '11.22.3' 
+    },
+    {
+      current: '11.22.4-beta.4',
+      expectedTarget: { version: { major: 11, minor: 22, patch: 8, suffix: undefined }, description: TargetVersionDescription.LATEST_PUBLISHED_PATCH_VERSION },
+      expectedNew: '11.22.9' 
+    },
+    {
+      current: '11.22.5-beta.2',
+      expectedTarget: { version: { major: 11, minor: 22, patch: 8, suffix: undefined }, description: TargetVersionDescription.LATEST_PUBLISHED_PATCH_VERSION },
+      expectedNew: '11.22.9' 
+    },
+    { 
+      current: '11.22.9-beta.1',
+      expectedTarget: { version: { major: 11, minor: 22, patch: 9, suffix: 'beta.1' }, description: TargetVersionDescription.LOCAL_VERSION },
+      expectedNew: '11.22.9' 
+    },
+    { 
+      current: '11.22.9-beta.3',
+      expectedTarget: { version: { major: 11, minor: 22, patch: 9, suffix: 'beta.3' }, description: TargetVersionDescription.LOCAL_VERSION },
+      expectedNew: '11.22.9' 
+    },
+  ];
+
+  testCases.forEach((t) => {
+    const provider = new PatchVersionProvider(t.current, publishedVersions);
     const tvInfo = provider.getTargetVersion();
     expect(tvInfo).toStrictEqual(t.expectedTarget);
 
--- src/version/version-providers/patch-version-provider.ts
+++ src/version/version-providers/patch-version-provider.ts
@@ -1,47 +1,99 @@
 import { VersionProvider, TargetVersionInfo, TargetVersionDescription } from '.';
 import VersionProviderBase from './version-provider-base';
-import { IszoleaVersionInfo } from '../version';
+import { IszoleaVersionInfo, VersionInfo } from '../version';
+import { parseIszoleaVersion } from '../version-parser';
 
 export default class PatchVersionProvider extends VersionProviderBase implements VersionProvider {
   getName(): string {
     return 'Patch';
   }
-  
+
   isCustom(): boolean {
     return false;
   }
 
   getNewVersion(): IszoleaVersionInfo | undefined {
-    const vi = this.versionInfo;
+    const targetVersion = this.getTargetVersion();
 
-    if (!vi) {
-      return undefined;
-    }
+    if (targetVersion) {
+      const vi = targetVersion.version;
 
-    let patch = vi.patch;
+      let patch = vi.patch;
 
-    if (!vi.suffix) {
-      patch = vi.patch + 1;
-    } else {
-      const regex = /beta.(\d+)/;
-      const match = vi.suffix.match(regex);
+      if (!vi.suffix) {
+        patch = vi.patch + 1;
+      } else {
+        const regex = /beta.(\d+)/;
+        const match = vi.suffix.match(regex);
 
-      if (!match || match.length < 2) {
-        return undefined;
+        if (!match || match.length < 2) {
+          return undefined;
+        }
+
+        patch = vi.patch;
       }
 
-      patch = vi.patch;
+      return {
+        major: vi.major,
+        minor: vi.minor,
+        patch,
+        betaIndex: undefined
+      };
     }
 
-    return {
-      major: vi.major,
-      minor: vi.minor,
-      patch,
-      betaIndex: undefined
-    };
+    return undefined;
   }
 
   getTargetVersion(): TargetVersionInfo | undefined {
+    const vi = parseIszoleaVersion(this.rawVersion);
+
+    if (vi) {
+      const targetPatchVersion = this.findTargetPatchVersion(vi);
+      if (targetPatchVersion) {
+        return targetPatchVersion;
+      }
+    }
     return this.versionInfo ? { version: this.versionInfo, description: TargetVersionDescription.LOCAL_VERSION } : undefined;
+  }
+
+  private findTargetPatchVersion(vi: IszoleaVersionInfo): TargetVersionInfo | undefined {
+    const currentPatch: IszoleaVersionInfo = {
+      major: vi.major,
+      minor: vi.minor,
+      patch: vi.patch,
+      betaIndex: undefined
+    };
+
+    const isCurrentPatchPublished = this.publishedVersions.some((v) => {
+      return v.isValid && v.parsedVersion && v.parsedVersion.major === currentPatch.major
+        && v.parsedVersion.minor === currentPatch.minor && v.parsedVersion.patch === currentPatch.patch
+        && v.parsedVersion.betaIndex === currentPatch.betaIndex;
+    });
+
+    if (isCurrentPatchPublished || vi.betaIndex === undefined) {
+      const latestPatchIndexes = this.publishedVersions.filter((v) => {
+        return v.isValid && v.parsedVersion && v.parsedVersion.major === vi.major
+          && v.parsedVersion.minor === vi.minor && v.parsedVersion.patch > vi.patch;
+      }).map((v) => v.parsedVersion && v.parsedVersion.patch);
+
+      const latestPatchIndex = latestPatchIndexes.reduce(
+        (prev, cur) => prev === undefined || (cur && cur > prev) ? cur : prev,
+        undefined
+      );
+
+      return latestPatchIndex === undefined
+        ? undefined
+        : {
+          version: {
+            major: vi.major,
+            minor: vi.minor,
+            patch: latestPatchIndex,
+            suffix: undefined
+          },
+          description: TargetVersionDescription.LATEST_PUBLISHED_PATCH_VERSION
+        };
+    }
+
+    return undefined;
   }
 }
