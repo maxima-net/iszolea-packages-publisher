8260b950e23dc2f224afacd717a9213334ca447f
Maxim Kucherov <maxima-net@bk.ru>
2020-05-26 19:50:48 +0300

    Add beta text providers

---
+++ src/version/beta-text-providers/iszolea.ts
@@ -0,0 +1,27 @@
+import { BetaTextProvider } from '.';
+
+
+export class IszoleaBetaTextProvider implements BetaTextProvider {
+  private static readonly PBI_SELECTOR_REGEX = /(pbi-\d+)/gmi;
+  private static readonly SPRINT_SELECTOR_REGEX = /^(sprint)$/gmi;
+
+  private readonly branchName: string;
+
+  constructor(branchName: string) {
+    this.branchName = branchName;
+  }
+
+  getText(): string | undefined {
+    const pbiMatchResult = this.branchName.match(IszoleaBetaTextProvider.PBI_SELECTOR_REGEX);
+    if (pbiMatchResult && pbiMatchResult.length >= 1) {
+      return `-${pbiMatchResult[0].toLowerCase()}`;
+    }
+
+    const sprintMatchResult = this.branchName.match(IszoleaBetaTextProvider.SPRINT_SELECTOR_REGEX);
+    if (sprintMatchResult && sprintMatchResult.length >= 1) {
+      return '-sprint';
+    }
+    
+    return undefined;
+  }
+}
